1c4ba3e5fc85aceb6ba6db146be3e23155e0ac72
Emmanuel Okafor <nondefyde@gmail.com>
2016-01-27 23:59:39 +0100

    The five crawlers has been completed. you can carry on

--- birdseye/pipelines.py
+++ birdseye/pipelines.py
@@ -26,26 +26,13 @@ class BirdseyePipeline(object):
              item['price'], item['stock_quantity'], item['product_url'], item['vendor'])
         )
 
-        # all this block run on it's own thread
-        # tx.execute("select * from product_tb where product_url = %s", (item['product_url'],))
-        # result = tx.fetchone()
-        # if result:
-        #     query = "UPDATE product_tb SET "
-        #     query += "product_name=%s, description=%s, manufacturer=%s, oem=%s,"
-        #     query += "price=%s, stock_quantity=%s, vendor=%s WHERE product_url=%s "
+        # query = "UPDATE product_tb SET "
+        # query += "product_name=%s, description=%s, manufacturer=%s, oem=%s,"
+        # query += "price=%s, stock_quantity=%s, vendor=%s WHERE product_url=%s "
         #
         #     tx.execute(query, (item['product_name'], item['description'], item['manufacturer'],
         #                        item['oem'], item['price'], item['stock_quantity'], item['vendor'], item['product_url']))
         #     logging.log(logging.INFO, "Item has  been updated in db: %s" % item)
-        # else:
-        #     tx.execute(
-        #         "insert into product_tb (product_name, description, manufacturer, oem,"
-        #         " price, stock_quantity, product_url, vendor)"
-        #         "values (%s, %s, %s, %s, %s, %s, %s, %s)",
-        #         (item['product_name'], item['description'], item['manufacturer'], item['oem'],
-        #          item['price'], item['stock_quantity'], item['product_url'], item['vendor'])
-        #     )
-        #     logging.log(logging.INFO, "Item stored in db: %s" % item)
 
     def handle_error(self, e):
         logging.log(logging.ERROR, e)
@@ -55,14 +42,13 @@ class JsonWriterPipeline(object):
     def __init__(self):
         self.file = open('items.json', 'wb')
 
-    def process_item(self, item,spider):
+    def process_item(self, item, spider):
         line = json.dumps(dict(item)) + ",\n"
         self.file.write(line)
         return item
 
 
 class JsonStartUrlWriterPipeline(object):
-
     def __init__(self):
         self.file = open('the url.json', 'wb')
 
