71a23544a3b4313bb4baa08c60e2497bb9b30696
Emmanuel Okafor <nondefyde@gmail.com>
2016-02-07 12:59:42 +0100

    The five crawlers has been completed. you can carry on

--- birdseye/spiders/westcmr_spider.py
+++ birdseye/spiders/westcmr_spider.py
@@ -8,7 +8,7 @@ class ShopspsSpider(scrapy.Spider):
     name = "west"
     allowed_domains = ["westcmr.com"]
     start_urls = [
-        # 'http://www.westcmr.com/american-medical-systems',
+        # 'http://www.westcmr.com/covidien'
         # 'http://www.westcmr.com/anspach'
     ]
 
@@ -20,10 +20,19 @@ class ShopspsSpider(scrapy.Spider):
             self.start_urls = self.start_urls + [url]
 
     def parse(self, response):
-        view_more_url = response.css('a.btn-view-more::attr(href)').extract()
-        if len(view_more_url) > 0:
-            more_page_url = 'http://www.westcmr.com' + view_more_url[0]
-            yield scrapy.Request(more_page_url, callback=self.parse)
+        last_page_no = response.css('input#LastPageNumber::attr(value)').extract()
+        print last_page_no
+        if len(last_page_no) > 0:
+            url = response.request.url + "?page="+last_page_no[0]
+        else:
+            url = response.request.url
+
+        request = scrapy.Request(url, callback=self.full_page_parse)
+        yield request
+
+    def full_page_parse(self, response):
+
+        print response.request.url
 
         urls = response.css('ul.unstyled li.product-title a::attr(href)').extract()
         for num in range(len(urls)):
@@ -33,8 +42,13 @@ class ShopspsSpider(scrapy.Spider):
             item['vendor'] = 'http://www.westcmr.com'
             request = scrapy.Request(item['url'], callback=self.parse_event, meta={'item': item})
             yield request
-
-        print response.request.url
+        #
+        # view_more_url = response.css('a.btn-view-more::attr(href)').extract()
+        # if len(view_more_url) > 0:
+        #     more_page_url = 'http://www.westcmr.com' + view_more_url[0]
+        #     yield scrapy.Request(more_page_url, callback=self.parse)
+        #
+        # print response.request.url
 
     def parse_event(self, response):
         sel = response.css('body')
@@ -43,6 +57,7 @@ class ShopspsSpider(scrapy.Spider):
         item['product_name'] = sel.css('div.row-fluid h1::text').extract()[0]
         oem = sel.css('div.row-fluid p::text').extract()[0]
         item['oem'] = oem.split(':')[1].strip()
+
         item['description'] = ''
         item['product_url'] = response.request.url
         item['stock_quantity'] = sel.css('div.boxshad table td::text').extract()[1].strip()
