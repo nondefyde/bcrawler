04da1c477eb8bebb14a7cc9a7852b5111ff1a6fb
Emmanuel Okafor <nondefyde@gmail.com>
2016-03-27 12:25:31 +0100

    The five crawlers has been completed. you can carry on

--- birdseye/spiders/esutures_spider.py
+++ birdseye/spiders/esutures_spider.py
@@ -6,7 +6,6 @@ from birdseye.items import BirdseyeItem
 class XsupplySpider(scrapy.Spider):
     name = "esu"
     allowed_domains = ["esutures.com"]
-    # start_urls = ['https://www.esutures.com/manufacturers/']
     start_urls = ['https://www.esutures.com/product/0-in-date/']
 
     def parse(self, response):
